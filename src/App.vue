2ae2cbfcc3be621c0f6573ddbc82ca5efbcae224
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-11-18 16:24:04 +0700

    base

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,72 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { computed, ref } from 'vue';
+import { parseDate } from 'src/utils/date';
+import Nature from './assets/image/nature.jpg';
+
+const title = ref('Wisuda');
+const date = ref(new Date('2024-05-13'));
+const now = ref(new Date());
+const diff = ref(date.value - now.value);
+
+const countdown = computed(() => {
+  return {
+    second: Math.floor((diff.value % (1000 * 60)) / 1000),
+    minute: Math.floor((diff.value % (1000 * 60 * 60)) / (1000 * 60)),
+    hour: Math.floor((diff.value % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    day: Math.floor(
+      (diff.value % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24),
+    ),
+    month: Math.floor(
+      (diff.value % (1000 * 60 * 60 * 24 * 30 * 12)) /
+        (1000 * 60 * 60 * 24 * 30),
+    ),
+    year: Math.floor(diff.value / (1000 * 60 * 60 * 24 * 30 * 12)),
+  };
+});
+
+setInterval(() => {
+  diff.value = date.value - new Date();
+}, 1000);
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div
+    class="flex items-center justify-center min-h-screen bg-cover"
+    :style="{
+      backgroundImage: `url(${Nature})`,
+    }"
+  >
+    <div class="fixed inset-0 bg-black bg-opacity-50"></div>
+    <div class="text-center text-white relative space-y-4">
+      <h1 class="text-2xl">
+        {{ title }} - {{ parseDate(date).format('LLL') }}
+      </h1>
+      <div class="flex gap-x-12 text-center justify-center">
+        <div v-if="countdown.year">
+          <p class="text-9xl font-bold">{{ countdown.year }}</p>
+          <p class="text-2xl">Tahun</p>
+        </div>
+        <div v-if="countdown.month">
+          <p class="text-9xl font-bold">{{ countdown.month }}</p>
+          <p class="text-2xl">Bulan</p>
+        </div>
+        <div>
+          <p class="text-9xl font-bold">{{ countdown.day }}</p>
+          <p class="text-2xl">Hari</p>
+        </div>
+        <div>
+          <p class="text-9xl font-bold">{{ countdown.hour }}</p>
+          <p class="text-2xl">Jam</p>
+        </div>
+        <div>
+          <p class="text-9xl font-bold">{{ countdown.minute }}</p>
+          <p class="text-2xl">Menit</p>
+        </div>
+        <div>
+          <p class="text-9xl font-bold">{{ countdown.second }}</p>
+          <p class="text-2xl">Detik</p>
+        </div>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/utils/date.js
@@ -0,0 +1,8 @@
+import dayjs from 'dayjs';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+
+dayjs.extend(localizedFormat);
+
+export function parseDate(date) {
+  return dayjs(date);
+}
